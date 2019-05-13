ca0c2387722513295bbcd7a2d2b9bf66bda9bb71
Arnaud Thiefaine <arnaud.thiefaine@gmail.com>
2019-05-13 21:13:26 +0200

    Replace Sulfuras conditions with a single guard condition

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -14,6 +14,10 @@ class GildedRose {
     }
 
     private void updateQuality(Item item) {
+        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            return;
+        }
+
         if (item.name.equals("Aged Brie")) {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
@@ -35,17 +39,11 @@ class GildedRose {
             }
         } else {
             if (item.quality > 0) {
-                if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                } else {
-                    item.quality = item.quality - 1;
-                }
+                item.quality = item.quality - 1;
             }
         }
 
-        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-        } else {
-            item.sellIn = item.sellIn - 1;
-        }
+        item.sellIn = item.sellIn - 1;
 
         if (item.sellIn < 0) {
             if (item.name.equals("Aged Brie")) {
@@ -57,9 +55,6 @@ class GildedRose {
                     item.quality = item.quality - item.quality;
                 } else {
                     if (item.quality > 0) {
-                        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            return;
-                        }
                         item.quality = item.quality - 1;
                     }
                 }
