116213752f8278aa07164e1db3f6bfd197be351d
Arnaud Thiefaine <arnaud.thiefaine@gmail.com>
2019-05-13 21:36:58 +0200

    Remove duplication on sellIn decrease statements

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -18,19 +18,18 @@ class GildedRose {
             return;
         }
 
+        item.sellIn = item.sellIn - 1;
+
         if (item.name.equals("Aged Brie")) {
             increaseQuality(item);
-            item.sellIn = item.sellIn - 1;
             if (item.sellIn < 0) increaseQuality(item);
         } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             increaseQuality(item);
 
-            item.sellIn = item.sellIn - 1;
             if (item.sellIn < 10) increaseQuality(item);
             if (item.sellIn < 5) increaseQuality(item);
             if (item.sellIn < 0) item.quality = item.quality - item.quality;
         } else {
-            item.sellIn = item.sellIn - 1;
             decreaseQuality(item);
             if (item.sellIn < 0) decreaseQuality(item);
         }
