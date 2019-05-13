2e8af5a215b25bf169a3789ea9df2724700e6643
Arnaud Thiefaine <arnaud.thiefaine@gmail.com>
2019-05-13 21:31:53 +0200

    Merge conditions based on item names

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -19,28 +19,20 @@ class GildedRose {
         }
 
         if (item.name.equals("Aged Brie")) {
-            item.sellIn = item.sellIn - 1;
             increaseQuality(item);
+            item.sellIn = item.sellIn - 1;
+            if (item.sellIn < 0) increaseQuality(item);
         } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             increaseQuality(item);
 
             if (item.sellIn < 11) increaseQuality(item);
             if (item.sellIn < 6) increaseQuality(item);
             item.sellIn = item.sellIn - 1;
+            if (item.sellIn < 0) item.quality = item.quality - item.quality;
         } else {
             item.sellIn = item.sellIn - 1;
             decreaseQuality(item);
-        }
-
-
-        if (item.name.equals("Aged Brie")) {
-            if (item.sellIn < 0) increaseQuality(item);
-        } else {
-            if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.sellIn < 0) item.quality = item.quality - item.quality;
-            } else {
-                if (item.sellIn < 0) decreaseQuality(item);
-            }
+            if (item.sellIn < 0) decreaseQuality(item);
         }
     }
 
