d452844d515eb467060fa0d0f395236bce4fa6f4
Arnaud Thiefaine <arnaud.thiefaine@gmail.com>
2019-05-13 21:34:19 +0200

    Trick on sellIn condition in order to move up sellIn decrease statement

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -26,8 +26,8 @@ class GildedRose {
             increaseQuality(item);
 
             if (item.sellIn < 11) increaseQuality(item);
-            if (item.sellIn < 6) increaseQuality(item);
             item.sellIn = item.sellIn - 1;
+            if (item.sellIn < 5) increaseQuality(item);
             if (item.sellIn < 0) item.quality = item.quality - item.quality;
         } else {
             item.sellIn = item.sellIn - 1;
