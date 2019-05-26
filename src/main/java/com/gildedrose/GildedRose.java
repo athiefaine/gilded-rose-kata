76213ea704db59fcdff46422213e36153e93a9e6
Arnaud Thiefaine <arnaud.thiefaine@gmail.com>
2019-05-26 17:55:10 +0200

    Duplicated full code for Sulfuras condition

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -50,6 +50,24 @@ class GildedRose {
             if (item.sellIn < 0) {
                 item.quality = item.quality - item.quality;
             }
+        } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (item.quality > 0) {
+                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    item.quality = item.quality - 1;
+                }
+            }
+
+            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                item.sellIn = item.sellIn - 1;
+            }
+
+            if (item.sellIn < 0) {
+                if (item.quality > 0) {
+                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                        item.quality = item.quality - 1;
+                    }
+                }
+            }
         } else {
             if (item.quality > 0) {
                 if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
